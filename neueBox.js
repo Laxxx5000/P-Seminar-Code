9e7d9a34f752fc0be02809491fd12c5e0a73bc45
Laxxx <scndgame5000@gmail.com>
2025-04-03 21:13:04 +0200

    Kleines Layout-Update+Asia Küche+ScriptFile

--- neueBox.js
+++ neueBox.js
@@ -13,3 +13,33 @@ function toggleBox() {
         arrow.classList.remove("rotated");
     }
 }
+function toggleBox2() {
+    const box = document.getElementById("toggleBox2");
+    const content = document.getElementById("content2");
+    const arrow = document.getElementById("arrow2");
+    
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+        box.classList.add("expanded");
+        arrow.classList.add("rotated");
+    } else {
+        content.style.display = "none";
+        box.classList.remove("expanded");
+        arrow.classList.remove("rotated");
+    }
+}
+function toggleBox3() {
+    const box = document.getElementById("toggleBox3");
+    const content = document.getElementById("content3");
+    const arrow = document.getElementById("arrow3");
+    
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+        box.classList.add("expanded");
+        arrow.classList.add("rotated");
+    } else {
+        content.style.display = "none";
+        box.classList.remove("expanded");
+        arrow.classList.remove("rotated");
+    }
+}
