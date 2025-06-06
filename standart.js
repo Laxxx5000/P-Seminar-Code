5ba5e3bd9049cf10f8f923bcab0c4dad922d4eba
Albi73 <schokolade7342@gmail.com>
2025-06-06 22:04:11 +0200

    Bayres und Bavarian auf englisch

--- standart.js
+++ standart.js
@@ -52,7 +52,8 @@ function goToRandomPageEN() {
     const subpages = [
       'Essen/en/asia(en).html',
       'Essen/en/backer(en).html',
-      'Essen/en/italian(en).html'
+      'Essen/en/italian(en).html',
+      'Essen/en/bayres(en).html',
     ];
   
     // Wähle eine zufällige Seite
